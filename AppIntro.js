2c4f3b5d373d10b1a036d6d5d4200e11daea85d0
Mohamad Arif Raja <rfraja2943@gmail.com>
2019-03-06 18:35:19 +0530

    Updated

--- AppIntro.js
+++ AppIntro.js
@@ -1,5 +1,6 @@
-import assign from 'assign-deep';
-import React, { Component, PropTypes } from 'react';
+import assign from "assign-deep";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import {
   StatusBar,
   StyleSheet,
@@ -9,154 +10,185 @@ import {
   Animated,
   Dimensions,
   Image,
-  Platform,
-} from 'react-native';
-import Swiper from 'react-native-swiper';
-import DoneButton from './components/DoneButton';
-import SkipButton from './components/SkipButton';
-import RenderDots from './components/Dots';
+  Platform
+} from "react-native";
+import Swiper from "react-native-swiper";
+import DoneButton from "./components/DoneButton";
+import SkipButton from "./components/SkipButton";
+import RenderDots from "./components/Dots";
 
-const windowsWidth = Dimensions.get('window').width;
-const windowsHeight = Dimensions.get('window').height;
+const windowsWidth = Dimensions.get("window").width;
+const windowsHeight = Dimensions.get("window").height;
 
 const defaulStyles = {
   header: {
     flex: 0.5,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   pic: {
     width: 150,
-    height: 150,
+    height: 150
   },
   info: {
     flex: 0.5,
-    alignItems: 'center',
-    padding: 30,
+    alignItems: "center",
+    padding: 30
   },
   slide: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-    padding: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#9DD6EB",
+    padding: 15
   },
   title: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 30,
-    paddingBottom: 20,
+    paddingBottom: 20
   },
   description: {
-    color: '#fff',
-    fontSize: 20,
+    color: "#fff",
+    fontSize: 20
   },
   controllText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 22,
-    fontWeight: 'bold',
+    fontWeight: "bold"
   },
   dotStyle: {
-    backgroundColor: 'rgba(255,255,255,.3)',
+    backgroundColor: "rgba(255,255,255,.3)",
     width: 13,
     height: 13,
     borderRadius: 7,
     marginLeft: 7,
     marginRight: 7,
     marginTop: 7,
-    marginBottom: 7,
+    marginBottom: 7
   },
   activeDotStyle: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   paginationContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 25,
     left: 0,
     right: 0,
-    flexDirection: 'row',
+    flexDirection: "row",
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent"
   },
   dotContainer: {
     flex: 0.6,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   },
   btnContainer: {
     flex: 0.2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50
   },
   nextButtonText: {
     fontSize: 25,
-    fontWeight: 'bold',
-    fontFamily: 'Arial',
+    fontWeight: "bold",
+    fontFamily: "Arial"
   },
   full: {
     height: 80,
     width: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-}
+    justifyContent: "center",
+    alignItems: "center"
+  }
+};
 
 export default class AppIntro extends Component {
+  static propTypes = {
+    dotColor: PropTypes.string,
+    activeDotColor: PropTypes.string,
+    rightTextColor: PropTypes.string,
+    leftTextColor: PropTypes.string,
+    onSlideChange: PropTypes.func,
+    onSkipBtnClick: PropTypes.func,
+    onDoneBtnClick: PropTypes.func,
+    onNextBtnClick: PropTypes.func,
+    pageArray: PropTypes.array,
+    doneBtnLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+    skipBtnLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+    nextBtnLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+    customStyles: PropTypes.object,
+    defaultIndex: PropTypes.number,
+    showSkipButton: PropTypes.bool,
+    showDoneButton: PropTypes.bool,
+    showDots: PropTypes.bool
+  };
+
+  static defaultProps = {
+    dotColor: "rgba(255,255,255,.3)",
+    activeDotColor: "#fff",
+    rightTextColor: "#fff",
+    leftTextColor: "#fff",
+    pageArray: [],
+    onSlideChange: () => {},
+    onSkipBtnClick: () => {},
+    onDoneBtnClick: () => {},
+    onNextBtnClick: () => {},
+    doneBtnLabel: "Done",
+    skipBtnLabel: "Skip",
+    nextBtnLabel: "›",
+    defaultIndex: 0,
+    showSkipButton: true,
+    showDoneButton: true,
+    showDots: true
+  };
   constructor(props) {
     super(props);
 
-    this.styles = StyleSheet.create(assign({}, defaulStyles, props.customStyles));
+    this.styles = StyleSheet.create(
+      assign({}, defaulStyles, props.customStyles)
+    );
 
     this.state = {
       skipFadeOpacity: new Animated.Value(1),
       doneFadeOpacity: new Animated.Value(0),
       nextOpacity: new Animated.Value(1),
-      parallax: new Animated.Value(0),
+      parallax: new Animated.Value(0)
     };
   }
 
-  onNextBtnClick = (context) => {
+  onNextBtnClick = context => {
     if (context.state.isScrolling || context.state.total < 2) return;
     const state = context.state;
     const diff = (context.props.loop ? 1 : 0) + 1 + context.state.index;
     let x = 0;
-    if (state.dir === 'x') x = diff * state.width;
-    if (Platform.OS === 'ios') {
+    if (state.dir === "x") x = diff * state.width;
+    if (Platform.OS === "ios") {
       context.refs.scrollView.scrollTo({ y: 0, x });
     } else {
       context.refs.scrollView.setPage(diff);
       context.onScrollEnd({
         nativeEvent: {
-          position: diff,
-        },
+          position: diff
+        }
       });
     }
     this.props.onNextBtnClick(context.state.index);
-  }
+  };
 
-  setDoneBtnOpacity = (value) => {
-    Animated.timing(
-      this.state.doneFadeOpacity,
-      { toValue: value },
-    ).start();
-  }
+  setDoneBtnOpacity = value => {
+    Animated.timing(this.state.doneFadeOpacity, { toValue: value }).start();
+  };
 
-  setSkipBtnOpacity = (value) => {
-    Animated.timing(
-      this.state.skipFadeOpacity,
-      { toValue: value },
-    ).start();
-  }
+  setSkipBtnOpacity = value => {
+    Animated.timing(this.state.skipFadeOpacity, { toValue: value }).start();
+  };
 
-  setNextOpacity = (value) => {
-    Animated.timing(
-      this.state.nextOpacity,
-      { toValue: value },
-    ).start();
-  }
+  setNextOpacity = value => {
+    Animated.timing(this.state.nextOpacity, { toValue: value }).start();
+  };
   getTransform = (index, offset, level) => {
     const isFirstPage = index === 0;
     const statRange = isFirstPage ? 0 : windowsWidth * (index - 1);
@@ -165,26 +197,31 @@ export default class AppIntro extends Component {
     const endOpacity = isFirstPage ? 1 : 1;
     const leftPosition = isFirstPage ? 0 : windowsWidth / 3;
     const rightPosition = isFirstPage ? -windowsWidth / 3 : 0;
-    const transform = [{
-      transform: [
-        {
-          translateX: this.state.parallax.interpolate({
-            inputRange: [statRange, endRange],
-            outputRange: [
-              isFirstPage ? leftPosition : leftPosition - (offset * level),
-              isFirstPage ? rightPosition + (offset * level) : rightPosition,
-            ],
-          }),
-        }],
-    }, {
-      opacity: this.state.parallax.interpolate({
-        inputRange: [statRange, endRange], outputRange: [startOpacity, endOpacity],
-      }),
-    }];
+    const transform = [
+      {
+        transform: [
+          {
+            translateX: this.state.parallax.interpolate({
+              inputRange: [statRange, endRange],
+              outputRange: [
+                isFirstPage ? leftPosition : leftPosition - offset * level,
+                isFirstPage ? rightPosition + offset * level : rightPosition
+              ]
+            })
+          }
+        ]
+      },
+      {
+        opacity: this.state.parallax.interpolate({
+          inputRange: [statRange, endRange],
+          outputRange: [startOpacity, endOpacity]
+        })
+      }
+    ];
     return {
-      transform,
+      transform
     };
-  }
+  };
 
   renderPagination = (index, total, context) => {
     let isDoneBtnShow;
@@ -204,61 +241,73 @@ export default class AppIntro extends Component {
     }
     return (
       <View style={[this.styles.paginationContainer]}>
-        {this.props.showSkipButton ? <SkipButton
-          {...this.props}
-          {...this.state}
-          isSkipBtnShow={isSkipBtnShow}
-          styles={this.styles}
-          onSkipBtnClick={() => this.props.onSkipBtnClick(index)} /> :
+        {this.props.showSkipButton ? (
+          <SkipButton
+            {...this.props}
+            {...this.state}
+            isSkipBtnShow={isSkipBtnShow}
+            styles={this.styles}
+            onSkipBtnClick={() => this.props.onSkipBtnClick(index)}
+          />
+        ) : (
           <View style={this.styles.btnContainer} />
-        }
-        {this.props.showDots && RenderDots(index, total, {
-          ...this.props,
-          styles: this.styles
-        })}
-        {this.props.showDoneButton ? <DoneButton
+        )}
+        {this.props.showDots &&
+          RenderDots(index, total, {
+            ...this.props,
+            styles: this.styles
+          })}
+        {this.props.showDoneButton ? (
+          <DoneButton
             {...this.props}
             {...this.state}
             isDoneBtnShow={isDoneBtnShow}
             styles={this.styles}
             onNextBtnClick={this.onNextBtnClick.bind(this, context)}
-            onDoneBtnClick={this.props.onDoneBtnClick} /> :
-            <View style={this.styles.btnContainer} />
-          }
+            onDoneBtnClick={this.props.onDoneBtnClick}
+          />
+        ) : (
+          <View style={this.styles.btnContainer} />
+        )}
       </View>
     );
-  }
+  };
 
-  renderBasicSlidePage = (index, {
-    title,
-    description,
-    img,
-    imgStyle,
-    backgroundColor,
-    fontColor,
-    level,
-  }) => {
+  renderBasicSlidePage = (
+    index,
+    { title, description, img, imgStyle, backgroundColor, fontColor, level }
+  ) => {
     const AnimatedStyle1 = this.getTransform(index, 10, level);
     const AnimatedStyle2 = this.getTransform(index, 0, level);
     const AnimatedStyle3 = this.getTransform(index, 15, level);
-    const imgSource = (typeof img === 'string') ? {uri: img} : img;
+    const imgSource = typeof img === "string" ? { uri: img } : img;
     const pageView = (
-      <View style={[this.styles.slide, { backgroundColor }]} showsPagination={false} key={index}>
-        <Animated.View style={[this.styles.header, ...AnimatedStyle1.transform]}>
+      <View
+        style={[this.styles.slide, { backgroundColor }]}
+        showsPagination={false}
+        key={index}
+      >
+        <Animated.View
+          style={[this.styles.header, ...AnimatedStyle1.transform]}
+        >
           <Image style={imgStyle} source={imgSource} />
         </Animated.View>
         <View style={this.styles.info}>
           <Animated.View style={AnimatedStyle2.transform}>
-            <Text style={[this.styles.title, { color: fontColor }]}>{title}</Text>
+            <Text style={[this.styles.title, { color: fontColor }]}>
+              {title}
+            </Text>
           </Animated.View>
           <Animated.View style={AnimatedStyle3.transform}>
-            <Text style={[this.styles.description, { color: fontColor }]}>{description}</Text>
+            <Text style={[this.styles.description, { color: fontColor }]}>
+              {description}
+            </Text>
           </Animated.View>
         </View>
       </View>
     );
     return pageView;
-  }
+  };
 
   renderChild = (children, pageIndex, index) => {
     const level = children.props.level || 0;
@@ -266,7 +315,9 @@ export default class AppIntro extends Component {
     const root = children.props.children;
     let nodes = children;
     if (Array.isArray(root)) {
-      nodes = root.map((node, i) => this.renderChild(node, pageIndex, `${index}_${i}`));
+      nodes = root.map((node, i) =>
+        this.renderChild(node, pageIndex, `${index}_${i}`)
+      );
     }
     let animatedChild = children;
     if (level !== 0) {
@@ -283,22 +334,33 @@ export default class AppIntro extends Component {
       );
     }
     return animatedChild;
-  }
+  };
 
   shadeStatusBarColor(color, percent) {
     const first = parseInt(color.slice(1), 16);
-    const black = first & 0x0000FF;
-    const green = first >> 8 & 0x00FF;
+    const black = first & 0x0000ff;
+    const green = (first >> 8) & 0x00ff;
     const percentage = percent < 0 ? percent * -1 : percent;
     const red = first >> 16;
     const theme = percent < 0 ? 0 : 255;
-    const finalColor = (0x1000000 + (Math.round((theme - red) * percentage) + red) * 0x10000 + (Math.round((theme - green) * percentage) + green) * 0x100 + (Math.round((theme - black) * percentage) + black)).toString(16).slice(1);
+    const finalColor = (
+      0x1000000 +
+      (Math.round((theme - red) * percentage) + red) * 0x10000 +
+      (Math.round((theme - green) * percentage) + green) * 0x100 +
+      (Math.round((theme - black) * percentage) + black)
+    )
+      .toString(16)
+      .slice(1);
 
     return `#${finalColor}`;
   }
 
   isToTintStatusBar() {
-    return this.props.pageArray && this.props.pageArray.length > 0 && Platform.OS === 'android'
+    return (
+      this.props.pageArray &&
+      this.props.pageArray.length > 0 &&
+      Platform.OS === "android"
+    );
   }
 
   render() {
@@ -309,21 +371,32 @@ export default class AppIntro extends Component {
     if (pageArray.length > 0) {
       pages = pageArray.map((page, i) => this.renderBasicSlidePage(i, page));
     } else {
-      if (Platform.OS === 'ios') {
-        pages = childrens.map((children, i) => this.renderChild(children, i, i));
+      if (Platform.OS === "ios") {
+        pages = childrens.map((children, i) =>
+          this.renderChild(children, i, i)
+        );
       } else {
         androidPages = childrens.map((children, i) => {
-          const { transform } = this.getTransform(i, -windowsWidth / 3 * 2, 1);
+          const { transform } = this.getTransform(
+            i,
+            (-windowsWidth / 3) * 2,
+            1
+          );
           pages.push(<View key={i} />);
           return (
-            <Animated.View key={i} style={[{
-              position: 'absolute',
-              height: windowsHeight,
-              width: windowsWidth,
-              top: 0,
-            }, {
-              ...transform[0],
-            }]}
+            <Animated.View
+              key={i}
+              style={[
+                {
+                  position: "absolute",
+                  height: windowsHeight,
+                  width: windowsWidth,
+                  top: 0
+                },
+                {
+                  ...transform[0]
+                }
+              ]}
             >
               {this.renderChild(children, i, i)}
             </Animated.View>
@@ -333,7 +406,10 @@ export default class AppIntro extends Component {
     }
 
     if (this.isToTintStatusBar()) {
-      StatusBar.setBackgroundColor(this.shadeStatusBarColor(this.props.pageArray[0].backgroundColor, -0.3), false);
+      StatusBar.setBackgroundColor(
+        this.shadeStatusBarColor(this.props.pageArray[0].backgroundColor, -0.3),
+        false
+      );
     }
 
     return (
@@ -345,14 +421,18 @@ export default class AppIntro extends Component {
           renderPagination={this.renderPagination}
           onMomentumScrollEnd={(e, state) => {
             if (this.isToTintStatusBar()) {
-              StatusBar.setBackgroundColor(this.shadeStatusBarColor(this.props.pageArray[state.index].backgroundColor, -0.3), false);
+              StatusBar.setBackgroundColor(
+                this.shadeStatusBarColor(
+                  this.props.pageArray[state.index].backgroundColor,
+                  -0.3
+                ),
+                false
+              );
             }
 
             this.props.onSlideChange(state.index, state.total);
           }}
-          onScroll={Animated.event(
-            [{ x: this.state.parallax }]
-          )}
+          onScroll={Animated.event([{ x: this.state.parallax }])}
         >
           {pages}
         </Swiper>
@@ -360,51 +440,3 @@ export default class AppIntro extends Component {
     );
   }
 }
-
-AppIntro.propTypes = {
-  dotColor: PropTypes.string,
-  activeDotColor: PropTypes.string,
-  rightTextColor: PropTypes.string,
-  leftTextColor: PropTypes.string,
-  onSlideChange: PropTypes.func,
-  onSkipBtnClick: PropTypes.func,
-  onDoneBtnClick: PropTypes.func,
-  onNextBtnClick: PropTypes.func,
-  pageArray: PropTypes.array,
-  doneBtnLabel: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element,
-  ]),
-  skipBtnLabel: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element,
-  ]),
-  nextBtnLabel: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element,
-  ]),
-  customStyles: PropTypes.object,
-  defaultIndex: PropTypes.number,
-  showSkipButton: PropTypes.bool,
-  showDoneButton: PropTypes.bool,
-  showDots: PropTypes.bool,
-};
-
-AppIntro.defaultProps = {
-  dotColor: 'rgba(255,255,255,.3)',
-  activeDotColor: '#fff',
-  rightTextColor: '#fff',
-  leftTextColor: '#fff',
-  pageArray: [],
-  onSlideChange: () => {},
-  onSkipBtnClick: () => {},
-  onDoneBtnClick: () => {},
-  onNextBtnClick: () => {},
-  doneBtnLabel: 'Done',
-  skipBtnLabel: 'Skip',
-  nextBtnLabel: '›',
-  defaultIndex: 0,
-  showSkipButton: true,
-  showDoneButton: true,
-  showDots: true
-};
